3c1d168457f9b6b252146407f8db189556732db3
GamzeYaman <71887367+GamzeYaman@users.noreply.github.com>
2023-05-01 15:04:51 +0300

    Send messages with key

--- kafka-demo-java/src/main/java/org/kafka/ProducerDemoKeys.java
+++ kafka-demo-java/src/main/java/org/kafka/ProducerDemoKeys.java
@@ -9,12 +9,12 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
-public class ProducerDemoWithCallback {
-    private static final Logger log = LoggerFactory.getLogger(ProducerDemoWithCallback.class.getSimpleName());
-    private static String topicName = "THIRD_TOPIC";
+public class ProducerDemoKeys {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoKeys.class.getSimpleName());
+    private static String topicName = "SECOND_TOPIC";
 
     public static void main(String[] args) {
-        log.info("I am Kafka Producer :)");
+        log.info("I am Kafka Producer");
 
         Properties properties = new Properties();
         properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
@@ -23,42 +23,36 @@ public class ProducerDemoWithCallback {
 
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
-        String members[] = {"Kim Namjoon", "Kim Seokjin", "Min Yoongi", "Jung Hoseok", "Park Jimin", "Kim Taehyung", "Jeon Jungkook"};
-        String message;
-
+        String members[] = {"Kim Jisoo", "Kim Jennie", "Park Chaeyoung", "Lalisa"};
+        String message, key;
         for(int j = 0; j < 3; j++){
-            for (int i = 0; i < members.length; i++){
-                message = members[i];
-                sendMessageToKafka(producer, message);
+            for(int i = 0; i < members.length; i++){
+                key = "id" + i;
+                message = members [i];
+                sendMessageToKafka(producer, key, message);
             }
             try {
-                Thread.sleep(5000);
+                Thread.sleep(4000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
 
-        producer.flush();
-        producer.close();
     }
 
-    private static void sendMessageToKafka(KafkaProducer<String, String> producer, String message) {
-        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName, message);
-
+    private static void sendMessageToKafka(KafkaProducer<String, String> producer, String key, String message) {
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName, key, message);
         producer.send(producerRecord, new Callback() {
             @Override
             public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                // executes every time a record successfully sent or an exception is thrown
-                if(e != null){
+                if(e != null) {
                     log.error("Error while producing! :( \n" +
                             "Problem : " + e.getMessage());
-                }else{
-                    log.info("Received new metadata! ;)\n" + "Topic: " + recordMetadata.topic() + "\t"
-                            + "Partition: " + recordMetadata.partition() + "\t"
-                            + "Offset: " + recordMetadata.offset() + "\t"
-                            + "Timestamp: " + recordMetadata.timestamp());
+                } else{
+                    log.info("Key: " + key + " | Partition: " + recordMetadata.partition() + "\t" +
+                            "Offset: " + recordMetadata.offset() + "\t" +
+                            "Timestamp: " + recordMetadata.timestamp());
                 }
-
             }
         });
     }
