8ced76170464e6fcc9f5d3689560cc3a1f723dfb
GamzeYaman <71887367+GamzeYaman@users.noreply.github.com>
2023-05-28 13:49:21 +0300

    Consumer implementation

--- kafka-consumer-opensearch/src/main/java/org/app/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/org/app/OpenSearchConsumer.java
@@ -1,21 +1,33 @@
 package org.app;
 
+import com.google.gson.JsonParser;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.opensearch.action.index.IndexRequest;
+import org.opensearch.action.index.IndexResponse;
 import org.opensearch.client.RequestOptions;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestHighLevelClient;
 import org.opensearch.client.indices.CreateIndexRequest;
 import org.opensearch.client.indices.GetIndexRequest;
+import org.opensearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
 
 public class OpenSearchConsumer {
 
@@ -52,6 +64,30 @@ public class OpenSearchConsumer {
             return restHighLevelClient;
         }
 
+       public static KafkaConsumer<String, String> createKafkaConsumer() {
+            String boostrapServers = "127.0.0.1:9092";
+            String groupId = "consumer-opensearch-demo";
+
+           Properties properties = new Properties();
+           properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServers);
+           properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+           properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+           properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+           properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+
+           return new KafkaConsumer<>(properties);
+
+       }
+
+       private static String extractId(String json){
+            return JsonParser.parseString(json)
+                    .getAsJsonObject()
+                    .get("meta")
+                    .getAsJsonObject()
+                    .get("id")
+                    .getAsString();
+       }
+
     public static void main(String[] args) throws IOException {
 
         Logger log = LoggerFactory.getLogger(OpenSearchConsumer.class.getSimpleName());
@@ -59,7 +95,9 @@ public class OpenSearchConsumer {
         //Opensearch client
         RestHighLevelClient openSearchClient  = createOpenSearchClient();
 
-        try(openSearchClient){
+        KafkaConsumer<String, String> consumer = createKafkaConsumer();
+
+        try(openSearchClient; consumer){
 
             boolean indexExists = openSearchClient.indices().exists(new GetIndexRequest("wikimedia"), RequestOptions.DEFAULT);
 
@@ -70,6 +108,36 @@ public class OpenSearchConsumer {
             }else {
                 log.info("The Wikimedia Index already exists.");
             }
+
+            consumer.subscribe(Collections.singleton("wikimedia.recentchange"));
+
+            while(true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(3000));
+
+                int recordCount = records.count();
+                log.info("Received " + recordCount + " records");
+
+                for(ConsumerRecord<String, String> record : records) {
+
+                    //Idempotence Strategy 1 : Define an ID using Kafka Record coordinates
+                    //String id = record.topic() + "_" + record.partition() + "_" + record.offset();
+
+                    //Strategy2
+                    //extract the id from the JSON
+                    String id = extractId(record.value());
+
+                    IndexRequest indexRequest = new IndexRequest("wikimedia")
+                            .source(record.value(), XContentType.JSON)
+                            .id(id);
+
+                    IndexResponse response = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
+
+                    log.info(response.getId());
+                }
+
+            }
+
+        } catch (Exception e) {
 
         }
 
